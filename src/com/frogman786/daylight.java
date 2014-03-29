eea6d3ee4da60b610e2f3de5a51070036ae9ac2f
frogman786 <frogman786@gmail.com>
2014-03-29 14:52:46 +1300

    added commands
    day dawn dusk noon and night they work but have no permissions yet

--- src/com/frogman786/daylight.java
+++ src/com/frogman786/daylight.java
@@ -3,12 +3,15 @@ package com.frogman786;
 import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class daylight extends JavaPlugin implements Listener {
 
@@ -28,9 +31,30 @@ public class daylight extends JavaPlugin implements Listener {
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args){
 		Player player = (Player) sender;
+		World world = player.getWorld();
+		if(lbl.equalsIgnoreCase("dawn")){
+			player.sendMessage(player.getPlayerListName() + ChatColor.WHITE +" Set " + ChatColor.DARK_GREEN + world + ChatColor.WHITE + "to dawn.");
+			world.setTime(23000);
+			return true;
+		}
 		if(lbl.equalsIgnoreCase("day")){
-			player.sendMessage(player.getPlayerListName() + "logged in, making" + getServer());
-			;
+			player.sendMessage(player.getPlayerListName() + ChatColor.WHITE +" Set " + ChatColor.DARK_GREEN + world + ChatColor.WHITE + "to day.");
+			world.setTime(0);
+			return true;
+		}
+		if(lbl.equalsIgnoreCase("noon")){
+			player.sendMessage(player.getPlayerListName() + ChatColor.WHITE +" Set " + ChatColor.DARK_GREEN + world + ChatColor.WHITE + "to noon.");
+			world.setTime(7000);
+			return true;
+		}
+		if(lbl.equalsIgnoreCase("dusk")){
+			player.sendMessage(player.getPlayerListName() + ChatColor.WHITE +" Set " + ChatColor.DARK_GREEN + world + ChatColor.WHITE + "to dusk.");
+			world.setTime(12500);
+			return true;
+		}
+		if(lbl.equalsIgnoreCase("night")){
+			player.sendMessage(player.getPlayerListName() + ChatColor.WHITE +" Set " + ChatColor.DARK_GREEN + world + ChatColor.WHITE + "to night.");
+			world.setTime(15000);
 			return true;
 		}
 		return false; 
