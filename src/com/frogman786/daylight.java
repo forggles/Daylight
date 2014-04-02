da5a64ed25f61770bf0abbeae8e95eae36fea5bc
frogman786 <frogman786@gmail.com>
2014-04-02 18:35:56 +1300

    my time commands more efficient
    added function time(Player player, String period, Integer time)
    and make checking if perms what command etc. one function calling others
    rather than 5 self contained.

--- src/com/frogman786/daylight.java
+++ src/com/frogman786/daylight.java
@@ -1,5 +1,6 @@
 package com.frogman786;
 
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
@@ -16,6 +17,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class daylight extends JavaPlugin implements Listener {
@@ -31,62 +33,65 @@ public class daylight extends JavaPlugin implements Listener {
 	public void onEnable(){
 		getServer().getPluginManager().registerEvents(this, this);
 		PluginDescriptionFile pdfFile = this.getDescription();
-		this.Log.info(pdfFile.getName() + pdfFile.getVersion() +  " Has Been Enabled! ");
+		this.Log.info(pdfFile.getName() + " " + pdfFile.getVersion() +  " Has Been Enabled! ");
 	}
 
+//	public HashMap<String, Boolean> platform = new HashMap<String, Boolean>();
+	
+//	public void togglePlatform(Player player) {
+//	    String playerName = player.getName();
+//	    if (platform.containsKey(playerName)) {
+//	        if (platform.get(playerName)) {
+//	        	platform.put(playerName, false);
+//	            player.sendMessage("Platform Toggle off");
+//	        } else {
+//	            platform.put(playerName, true);
+//	            player.sendMessage("Platform Toggle on");
+//	        }
+//	    } else {
+//	        platform.put(playerName, false); //If you want plugin disabled by default change this value to false.
+//	        player.sendMessage("Platform Toggle off");
+//	    }
+//	}
+	
+	public void time(Player player, String period, Integer time){
+		World world = player.getWorld();
+		getServer().broadcastMessage(player.getPlayerListName() + ChatColor.WHITE +" Set " + ChatColor.DARK_GREEN + world.getName() + ChatColor.WHITE + " to " + period + ".");
+		world.setTime(time);
+	}
+	
+	
 	public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args){
 		Player player = (Player) sender;
 		World world = player.getWorld();
-		if(lbl.equalsIgnoreCase("dawn")){
-			if(player.hasPermission("frog.time.dawn")){
-				getServer().broadcastMessage(player.getPlayerListName() + ChatColor.WHITE +" Set " + ChatColor.DARK_GREEN + player.getWorld().getName() + ChatColor.WHITE + " to dawn.");
-				world.setTime(23000);
-				return true;
+		int period = 0;
+		if(lbl.equalsIgnoreCase("dawn")|lbl.equalsIgnoreCase("day")|lbl.equalsIgnoreCase("noon")|lbl.equalsIgnoreCase("dusk")|lbl.equalsIgnoreCase("night")){
+			if(player.hasPermission("frog.time." + lbl)){
+				if(lbl == "dawn"){
+					period = 23000;
+				}else{
+					if(lbl == "day"){
+						period = 0;
+					}else{
+						if(lbl == "noon"){
+							period = 7000;
+						}else{
+							if(lbl == "dusk"){
+								period = 12500;
+							}else{
+								period = 15000;
+							}
+						}
+					}
+				}
+			time(player, lbl, period);
+			return true;
 			}else{
 				player.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
 				return true;
 			}
 		}
-		if(lbl.equalsIgnoreCase("day")){
-			if(player.hasPermission("frog.time.day")){
-				getServer().broadcastMessage(player.getPlayerListName() + ChatColor.WHITE +" Set " + ChatColor.DARK_GREEN + player.getWorld().getName() + ChatColor.WHITE + " to day.");
-				world.setTime(0);
-				return true;
-			}else{
-				player.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
-				return true;
-			}
-		}
-		if(lbl.equalsIgnoreCase("noon")){
-			if(player.hasPermission("frog.time.noon")){
-				getServer().broadcastMessage(player.getPlayerListName() + ChatColor.WHITE +" Set " + ChatColor.DARK_GREEN + player.getWorld().getName() + ChatColor.WHITE + " to noon.");
-				world.setTime(7000);
-				return true;
-			}else{
-				player.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
-				return true;
-			}
-		}
-		if(lbl.equalsIgnoreCase("dusk")){
-			if(player.hasPermission("frog.time.dusk")){
-				getServer().broadcastMessage(player.getPlayerListName() + ChatColor.WHITE +" Set " + ChatColor.DARK_GREEN + player.getWorld().getName() + ChatColor.WHITE + " to dusk.");
-				world.setTime(12500);
-				return true;
-			}else{
-				player.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
-				return true;
-			}
-		}
-		if(lbl.equalsIgnoreCase("night")){
-			if(player.hasPermission("frog.time.night")){
-				getServer().broadcastMessage(player.getPlayerListName() + ChatColor.WHITE +" Set " + ChatColor.DARK_GREEN + player.getWorld().getName() + ChatColor.WHITE + " to night.");
-				world.setTime(15000);
-				return true;
-			}else{
-				player.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
-				return true;
-			}
-		}
+
 		if(lbl.equalsIgnoreCase("who")){
 			if(player.hasPermission("frog.who.who")){
 				player.sendMessage("there are some people online");
@@ -107,13 +112,17 @@ public class daylight extends JavaPlugin implements Listener {
 		}
 		//NEED TO ADD REGION AND AIR CHECKING TO THIS
 		if(lbl.equalsIgnoreCase("platform")){
-			if(player.hasPermission("frog.platform.single")){
+			if(player.hasPermission("frog.platform.single") & args[0] == null){
 			    Location loc = player.getPlayer().getLocation();
 			    loc.setY(loc.getY() - 1);
 			    Block b = loc.getBlock();
 			    b.setType(Material.GLASS);
 				player.sendMessage(ChatColor.DARK_GREEN + "Platform created.");
 			return true;
+//			}
+//		    if(player.hasPermission("frog.platform.toggle") & args[0] == "toggle"){
+//				togglePlatform(player);
+//				return true;
 			}else{
 				player.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
 				return true;
@@ -122,6 +131,8 @@ public class daylight extends JavaPlugin implements Listener {
 		return false; 
 	
 	}
+
+
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent evt) {
 	    Player player = evt.getPlayer();
@@ -132,4 +143,14 @@ public class daylight extends JavaPlugin implements Listener {
 	    Player player = evt.getPlayer();
 	    evt.setQuitMessage(player.getPlayerListName() + ChatColor.WHITE + " quit " + ChatColor.RED + (Bukkit.getOnlinePlayers().length - 1) + ChatColor.GREEN + " players" + ChatColor.WHITE + " left");
 	}
+//	@EventHandler
+//	public void onPlayerMove(PlayerMoveEvent evt) {
+//	    Player player = (Player) evt.getPlayer();
+//	    if(platform.containsKey(player) & platform.get(player)){
+//	    	Location loc = evt.getPlayer().getLocation();
+//	    	loc.setY(loc.getY() - 1);
+//	    	Block b = loc.getBlock();
+//	    	b.setType(Material.GLASS);
+//		}
+//	}
 }
